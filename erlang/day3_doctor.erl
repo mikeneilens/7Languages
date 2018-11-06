bb7827f028ba8a9af3dbe85763203499110d48b3
Mike Neilens <mike.neilens@gmail.com>
2018-11-06 18:59:52 +0000

    day3 of erlang, problems 1 and 2.

---
+++ erlang/day3_doctor.erl
@@ -0,0 +1,33 @@
+-module(day3_doctor).
+-export([loop/0, monitor/0]).
+
+loop() -> 
+	process_flag(trap_exit, true),
+	link(self()),
+	receive
+		new ->
+			io:format("Creating and monitoring process. ~n"),
+			register(revolver, spawn_link(fun roulette:loop/0)),
+			loop();
+		suicide -> 
+			io:format("I just killed myself! ~n"),
+			exit("Suicide is painless!");	
+		{'EXIT', From, Reason} ->
+			io:format("The shooter ~p died with reason ~p . ",[From, Reason]),
+			self() ! new,
+			loop()
+	end.
+
+monitor() ->
+	process_flag(trap_exit, true),
+	receive
+		new ->
+			io:format("Creating a new doctor. ~n"),
+			register(doctor, spawn_link(fun loop/0)),
+			doctor ! new,				
+			monitor();	
+		{'EXIT', From, Reason} ->
+				io:format("The doctor ~p just died with reason ~p ~n",[From,Reason]),
+				self() ! new,
+				monitor()
+	end.
---
+++ erlang/day3_translate.erl
@@ -0,0 +1,37 @@
+-module(day3_translate).
+-export([loop/0, translate/2, monitor/0]).
+
+loop() ->
+	receive 
+		{From,"casa"} ->
+			From ! "house",
+			loop();
+		{From, "blanca"} ->
+			From ! "white",
+			loop();
+		{From, "suicidio"} ->
+			From ! "I just killed myself",
+			exit("Bye, bye!");
+		{From, _} -> 
+			From ! "I don't understand ",
+			loop()
+	end.
+
+	translate(To, Word) -> 
+		To ! {self(), Word}, 
+		receive 
+			Translation -> Translation
+		end.
+
+	monitor() ->
+		process_flag(trap_exit, true),
+		receive
+			new ->
+				io:format("Creating a new translator. ~n"),
+				register(translator, spawn_link(fun loop/0)),
+				monitor();
+			{'EXIT', From, Reason} ->
+				io:format("The translator ~p just died with reason ~p",[From,Reason]),
+				self() ! new,
+				monitor()
+		end.

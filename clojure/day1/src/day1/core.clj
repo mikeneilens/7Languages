950f6cffecdd5b27ff17dfb89abd41e494729685
Mike Neilens <mike.neilens@gmail.com>
2019-01-17 19:23:07 +0000

    clojure day1

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -1,7 +1,7 @@
 (ns day1.core)
-(defn big [[st n]] (> (count st ) n))
+(defn big [st n] (> (count st ) n))
 
-(defn collection-type [col] (cond   (= (type col) "clojure.lang.PersistentVector") "Vector" 
-                                    (= (type col) "clojure.lang.PersistentList") "List" 
+(defn collection-type [col] (cond   (= (str (type col)) "class clojure.lang.PersistentVector") "Vector" 
+                                    (= (str (type col)) "class clojure.lang.PersistentList") "List" 
                                     :else "something else")
 )

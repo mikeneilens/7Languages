728ecc61e54113efc8d6a0540c2d3cf96c217fc0
Mike Neilens <mike.neilens@gmail.com>
2019-01-18 08:42:47 +0000

    clojure day1

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -5,7 +5,9 @@
   "A vresion of big that uses a Let to bind stringLength to (count st) before executing the logic"
   (let [stringLength (count st) nCharacters n]  (> stringLength nCharacters ) ))
 
-(defn collection-type [col] (cond   (= (str (type col)) "class clojure.lang.PersistentVector") "Vector" 
-                                    (= (str (type col)) "class clojure.lang.PersistentList") "List" 
-                                    :else "something else")
-)
+(defn collection-type [col] (let[ className (str (type col)) ]   
+                               (cond   (= className "class clojure.lang.PersistentVector") "Vector" 
+                                       (= className "class clojure.lang.PersistentList") "List" 
+                                        :else "something else")
+                             )
+ )
